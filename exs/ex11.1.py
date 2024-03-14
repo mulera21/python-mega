734d75f5a31be31f5cadc26e729dc5d0ee5a3e82
edmond <edmondmulera55@gmail.com>
2024-03-14 14:36:51 +0300

    add complete and exit button

---
+++ exs/ex11.1.py
@@ -0,0 +1,16 @@
+"""
+The function expects the input value to be in ounces.
+
+To convert ounces to milliliters, multiply the value in ounces
+ by the conversion factor 29.57353.
+Make sure to return the result of
+the multiplication as the output of the function.
+"""
+
+
+def foo(met):
+    ne_met = met
+    nice = 29.57353
+
+    results = ne_met * nice
+    return results
--- gui_todo app/cli.py
+++ gui_todo app/cli.py
@@ -7,9 +7,11 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("complete")
+exit_button = sg.Button("exit")
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 while True:
     event, values = window.read()
@@ -33,6 +35,18 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window['todos'].update(values="")
+
+        case "exit":
+            break
+
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case sg.WIN_CLOSED:
