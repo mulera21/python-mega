4b8ab322be81408324facc4131da9a998ac041c2
ikumwa edmond mulera <edmondmulera55@gmail.com>
2024-03-15 15:42:09 +0300

    webcam app

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from PIL import Image
+st.subheader("Color to Grayscale Converter")
+
+# Create a file uploader component allowing the user to upload a file
+uploaded_image = st.file_uploader("Upload Image")
 
 with st.expander("start camera"):
     # start the camera
@@ -12,3 +16,12 @@ if camera_image:
     # we convert the image to gray scale
     gray_img = img.convert("L")
     st.image(gray_img)
+
+# Check if the image exists meaning the user has uploaded a file
+if uploaded_image:
+    # Open the user uploaded image with PIL
+    img = Image.open(uploaded_image)
+    # Convert the image to grayscale
+    gray_uploaded_img = img.convert('L')
+    # Display the grayscale image on the webpage
+    st.image(gray_uploaded_img)
